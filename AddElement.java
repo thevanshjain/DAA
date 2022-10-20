4002d439b89a666483a42676a7bc5720f92f4548
jainaincy <116289259+jainaincy@users.noreply.github.com>
2022-10-20 20:11:33 +0530

    Create AddElement.java

---
+++ AddElement.java
@@ -0,0 +1,25 @@
+
+class Solution {
+	ListNode addAtkthElement (ListNode head, int k, ListNode newElement) {
+	    // add your logic here
+		int count=1;
+		ListNode cur=head;
+		ListNode temp;
+		if(k==1){
+			newElement.next=head;
+			return newElement;
+		}
+		while(cur!=null){
+			if(k-1==count){
+				temp=cur.next;
+				cur.next=newElement;
+				newElement.next=temp;
+				return head;
+			}
+			count++;
+			cur=cur.next;
+			
+		}
+		return head;
+	}
+}
